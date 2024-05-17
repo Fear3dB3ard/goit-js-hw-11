f05339b2726086deaac1e77642a58fda52fbc184
Fear3dB3ard <craciunstefanmarian@yahoo.com>
2024-05-17 12:12:12 +0300

    Fix build issues by adding buffer module

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import axios from 'axios';
 import Notiflix from 'notiflix';
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
+import { Buffer } from 'buffer';
 
 const form = document.getElementById('search-form');
 const gallery = document.querySelector('.gallery');
